d8ad167581766cfe24f8c03d82e734d640637b63
Sven Steinheißer <sven.steinheisser@gmail.com>
2023-10-17 21:19:22 +0200

    Added ExpenseFilter and listening and propagatingto changes

--- expense-calculator/src/components/Expenses/ExpenseList.js
+++ expense-calculator/src/components/Expenses/ExpenseList.js
@@ -1,10 +1,19 @@
+import React, { useState } from 'react';
 import Card from '../UI/Card';
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 import './ExpenseList.css';
 
-const ExpenseList = (props) => {
+const ExpenseList = props => {
+    const [selectedYear, setSelectedYear] = useState('2020');
+
+    const filterChangedHandler = selectedYear => {
+        console.log(selectedYear);
+        setSelectedYear(selectedYear);
+    };
     return (
         <Card className='expenses'>
+            <ExpensesFilter selectedYear={selectedYear} onFilterChanged={filterChangedHandler} />
             {props.expenses.map((expense) => <ExpenseItem key={expense.id} expense={expense} />)}
         </Card>
     );
