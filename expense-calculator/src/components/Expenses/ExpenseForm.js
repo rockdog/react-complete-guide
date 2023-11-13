0b01d31b697975ce9f569259359f13eef2f83bf3
Sven Steinheißer <sven.steinheisser@gmail.com>
2023-11-13 15:51:06 +0100

    Render form conditionally

--- expense-calculator/src/components/Expenses/ExpenseForm.js
+++ expense-calculator/src/components/Expenses/ExpenseForm.js
@@ -45,6 +45,7 @@ const ExpenseForm = (props) => {
                 </div>
             </div>
             <div className='new-expense__actions'>
+                <button type='button' onClick={props.onCancel}>Cancel</button>
                 <button type='submit' onClick={submitHandler}>Add Expense</button>
             </div>
         </form>
