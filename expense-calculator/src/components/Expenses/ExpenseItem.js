66b2edd7798cc08ef072a617164a657c28d37e12
Sven Steinheißer <sven.steinheisser@gmail.com>
2023-11-13 15:25:37 +0100

    Add missing li tag

--- expense-calculator/src/components/Expenses/ExpenseItem.js
+++ expense-calculator/src/components/Expenses/ExpenseItem.js
@@ -7,13 +7,15 @@ const ExpenseItem = (props) => {
     const amount = props.expense.amount;
     const date = props.expense.date;
     return (
-        <Card className='expense-item'>
-            <ExpenseDate date={date} />
-            <div className='expense-item__description'>
-                <h2>{title}</h2>
-                <div className='expense-item__price'>€{amount}</div>
-            </div>
-        </Card>
+        <li>
+            <Card className='expense-item'>
+                <ExpenseDate date={date} />
+                <div className='expense-item__description'>
+                    <h2>{title}</h2>
+                    <div className='expense-item__price'>€{amount}</div>
+                </div>
+            </Card>
+        </li>
     );
 }
 
